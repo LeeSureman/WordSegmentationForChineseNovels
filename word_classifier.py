9a33caf630f20760915fc83341f2d06a58f0bb47
LeeSureman <1349342500@QQ.com>
2019-03-28 09:51:53 +0800

    change for qiu

--- word_classifier.py
+++ word_classifier.py
@@ -9,6 +9,7 @@ from sklearn.metrics import classification_report,precision_score,recall_score,f
 from data_preprocess import *
 import copy
 import math
+from enhanced_tagger_1 import EnhancedTagger
 
 np.set_printoptions(suppress=True, threshold=np.nan)
 
@@ -565,6 +566,22 @@ if __name__ == '__main__':
     # parser.add_argument('--pku_dict',default=r'D:\PycharmProjects\WordSegmentationForChineseNovels\data\qiu_dict.txt')
     parser.add_argument('--gold',default=r'D:\PycharmProjects\WordSegmentationForChineseNovels\data\novel\zx_300_dev.txt')
     # parser.add_argument('--auto_tagged',default=r'D:\PycharmProjects\WordSegmentationForChineseNovels\data\novel\zx_300_dev.txt')
+    parser.add_argument('--data_seed',default=-1,help='how to seg data train/test')
+
+    parser.add_argument('--test', default=None, help='if mode is test,it is annotated,if mode is tag,it is raw')
+    parser.add_argument('--base_weight',required=True,default=None,help='base weight')
+    parser.add_argument('--enhanced_weight',required=True,help='enhanced weight')
+    parser.add_argument('--dataset_test',help='ctb or pku or novel')
+    # parser.add_argument('--mode',help='train or test')
+    parser.add_argument('--pku_dict',help='the common word in pku dict',required=True)
+    parser.add_argument('--add',default='')
+    parser.add_argument('--dataset_train', help='ctb or pku')
+    parser.add_argument('--new_feature',default='1')
+    # parser.add_argument('--record',default=None)
+    parser.add_argument('--use_pattern_feature',default=False,help='whether use pattern feature')
+
+    parser.add_argument('--output_tagged',help='the path of the tagged sentenced',default=None)
+    parser.add_argument('--is_raw',default=False,help='the data format when tagging',type=bool)
 
     args = parser.parse_args()
     w_c = WordClassifier(args.train,use_start_end_pmi=True,use_lisan=False,use_punc=True,
@@ -663,6 +680,8 @@ if __name__ == '__main__':
 
     precision = len(true_w)/len(w_c.W)
     recall = len(true_w)/len(all_new_noun)
+    if ' ' in w_c.W:
+        w_c.W.remove(' ')
     print('we:',len(w_c.W))
     print('gold:',len(all_new_noun))
     print('r:',recall,'p:',precision)
@@ -702,6 +721,189 @@ if __name__ == '__main__':
     for w in w_c.W:
         w2freq[len(w)] = w2freq.setdefault(len(w),0)+1
     print(w2freq)
+
+
+
+    b_t = BaseTagger(args)
+    e_t = EnhancedTagger(args)
+
+    if args.mode =='test':
+
+        train,test = loadQiuPKU(args.train,args.data_seed)
+
+
+        novel_test = loadNovelData(args.test)
+
+
+        b_t.prepareKnowledge(train)
+        b_t.weight.weightDict = pickle.load(open(args.base_weight,'rb'))
+
+
+
+        # n_count = 0
+        # genCorpus_word = set()
+        # for i in range(len(train[0])):
+        #     for j in range(len(train[0][i])):
+        #         genCorpus_word.add(train[0][i][j])
+
+        # auto_split = []
+        # W_candidate = set()
+        # for i in range(len(novel_test[0])):
+        #     now_state = b_t.tag(novel_test[2][i],False,b_t.judge_by_rule(novel_test[2][i]))
+        #     auto_split.append(now_state)
+        #     for i in range(2,len(now_state.word)-1):
+        #         if now_state.word[i] not in genCorpus_word:
+        #             W_candidate.add(now_state.word[i]+str(int(now_state.tag[i-1]=='w'and now_state.tag[i+1] =='w')))
+
+        # print(W_candidate)
+
+
+        e_t.prepareKnowledge(train)
+        e_t.weight.weightDict = pickle.load(open(args.enhanced_weight,'rb'))
+
+        e_t.W = w_c.W
+        e_t.P = w_c.P
+
+        novel_gold_state = []
+        for i in range(len(novel_test[0])):
+            novel_gold_state.append(State(novel_test[0][i], novel_test[1][i], isGold=True))
+
+
+        b_result = b_t.test(novel_test[2],novel_gold_state)
+
+        e_result = e_t.test(novel_test[2],novel_gold_state)
+
+        print('base tagger:',b_result)
+        print('\nenhanced tagger:',e_result)
+
+        # print('w:',e_t.wc.lr.w)
+        # print('b:',e_t.wc.lr.b)
+
+    elif args.mode =='tag' :
+        train,test = loadQiuPKU(args.train,args.data_seed)
+
+        if args.is_raw:
+            # print('israw!')
+            novel_fp = open(args.test, 'r', encoding='utf-8')
+            novel_raw = novel_fp.readlines()
+
+        else:
+            # print('is not raw!')
+            novel_test = loadNovelData(args.test)
+        # novel_fp = open(args.test,'r',encoding='utf-8')
+            novel_raw = novel_test[2]
+
+
+        # for line in novel_raw[:10]:
+        #     print(line)
+        # exit(2)
+
+        for i,line in enumerate(novel_raw):
+            novel_raw[i] = line.strip()
+
+
+        b_t.prepareKnowledge(train)
+        b_t.weight.weightDict = pickle.load(open(args.base_weight,'rb'))
+
+
+
+        # n_count = 0
+        genCorpus_word = set()
+        for i in range(len(train[0])):
+            for j in range(len(train[0][i])):
+                genCorpus_word.add(train[0][i][j])
+
+        auto_split = []
+        W_candidate = set()
+        for i in range(len(novel_raw)):
+            now_state = b_t.tag(novel_raw[i],False,b_t.judge_by_rule(novel_raw[i]))
+            auto_split.append(now_state)
+            for i in range(2,len(now_state.word)-1):
+                if now_state.word[i] not in genCorpus_word:
+                    W_candidate.add(now_state.word[i]+str(int(now_state.tag[i-1]=='w'and now_state.tag[i+1] =='w')))
+
+        # print(W_candidate)
+
+
+        e_t.prepareKnowledge(train)
+        e_t.weight.weightDict = pickle.load(open(args.enhanced_weight,'rb'))
+
+
+        e_t.W = set()
+
+        for p in e_t.P:
+            a,b = p.split('_')
+            onNewPattern(e_t,auto_split,a,b,W_candidate)
+
+
+        # novel_gold_state = []
+        # for i in range(len(novel_test[0])):
+        #     novel_gold_state.append(State(novel_test[0][i], novel_test[1][i], isGold=True))
+
+        # old_result = e_t.test(novel_test[2],novel_gold_state)
+
+        # print('general know:',old_result)
+        W_candidate_copy = copy.deepcopy(W_candidate)
+        noun_tag_set = {'ns','nz','n','nr','nt'}
+        novel_noun_set = set()
+        for i,s in enumerate(test[0]):
+            for j,w in enumerate(test[0][i]):
+                if test[1][i][j] in noun_tag_set:
+                    novel_noun_set.add(test[0][i][j])
+
+        while True:
+            n_count = 0
+            for w in W_candidate:
+                if w[-1] == '1':
+                    t = ['w','w','w']
+                    s = State(['',w[:-1],''],t,True)
+                else:
+                    t = ['','','']
+                    s = State(['',w[:-1],''],t,True)
+
+                isNewNoun = e_t.wc.tag(s,3)
+                # print(w,isNewNoun)
+
+                if isNewNoun>0.5:
+                    print(w, isNewNoun)
+                    W_candidate_copy.remove(w)
+                    e_t.W.add(w[:-1])
+                    n_count+=1
+                    for i in range(len(auto_split)):
+                        for j in range(2,len(auto_split[i].word)-1):
+                            if auto_split[i].word[j] == w[:-1]:
+                                if auto_split[i].word[j-1]+'_'+auto_split[i].word[j+1] not in e_t.P:
+                                    onNewPattern(e_t,auto_split,auto_split[i].word[j-1],
+                                                 auto_split[i].word[j+1],W_candidate)
+            print('\n************\n')
+            if n_count == 0:
+                break
+            n_count = 0
+            W_candidate = copy.deepcopy(W_candidate_copy)
+
+        if args.output_tagged==None:
+            f = open('tmp_result','a',encoding='utf-8')
+            print('********new tag result********',file=f)
+        else:
+            f = open(args.output_tagged,'w',encoding='utf-8')
+        for line in novel_raw:
+            tmp_r = e_t.tag(line,False,e_t.judge_by_rule(line))
+            now_line = []
+            for i,w in enumerate(tmp_r.word):
+                if tmp_r.tag[i]!='PAD' and w!=' ':
+                    now_line.append(w+'_'+tmp_r.tag[i])
+            print(' '.join(now_line),file=f)
+        # noun_acc = len(novel_noun_set & e_t.W) / len(e_t.W)
+
+        # b_result = b_t.test(novel_test[2],novel_gold_state)
+
+        # e_result = e_t.test(novel_test[2],novel_gold_state)
+
+        # print('noun acc:',noun_acc)
+
+
+        # print('base tagger:',b_result)
+        # print('\nenhanced tagger:',e_result)
 
 
 
