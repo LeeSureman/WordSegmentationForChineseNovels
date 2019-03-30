c15d2abd449f923511b03eaa4f26091e09ac761a
LeeSureman <1349342500@QQ.com>
2019-03-30 20:28:02 +0800

    change for qiu

--- word_classifier.py
+++ word_classifier.py
@@ -551,7 +551,7 @@ def has_no_punc(w,puncs=set()):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--train',help='the path of the training set')
-    parser.add_argument('--auto_tagged',help='the path of the auto tagged novel')
+    parser.add_argument('--auto_tagged',default=None,help='the path of the auto tagged novel')
     # parser.add_argument('--pku_dict',default=r'D:\PycharmProjects\WordSegmentationForChineseNovels\data\qiu_dict.txt')
     parser.add_argument('--gold',help='the path of the gold novel')
     # parser.add_argument('--auto_tagged',default=r'D:\PycharmProjects\WordSegmentationForChineseNovels\data\novel\zx_300_dev.txt')
@@ -580,8 +580,24 @@ if __name__ == '__main__':
                          limit=0.5)
     w_c.prepare_training_data_and_train()
 
-    wordss,tagss,sentences = loadNovelData(args.auto_tagged)
+    b_t = BaseTagger(args)
+    b_t.prepareKnowledge(train)
+    b_t.weight.weightDict = pickle.load(open(args.base_weight, 'rb'))
+
+
+
+    # wordss,tagss,sentences = loadNovelData(args.auto_tagged)
+
     gold_wordss,gold_tagss,gold_sentences = loadNovelData(args.gold)
+
+    states = []
+    wordss = []
+    tagss = []
+    sentences = gold_sentences
+    for s in gold_sentences:
+        tmp_state = b_t.tag(s,False,b_t.judge_by_rule(s))
+        wordss.append(tmp_state.word[2:-1])
+        tagss.append(tmp_state.tag[2:-1])
     for words in wordss:
         for w in words:
             if w not in w_c.gen_set and w not in w_c.w_candidate_info:
@@ -713,7 +729,6 @@ if __name__ == '__main__':
 
 
 
-    b_t = BaseTagger(args)
     e_t = EnhancedTagger(args)
 
     if args.mode =='test':
@@ -722,6 +737,75 @@ if __name__ == '__main__':
 
 
         novel_test = loadNovelData(args.test)
+
+
+
+
+        # n_count = 0
+        # genCorpus_word = set()
+        # for i in range(len(train[0])):
+        #     for j in range(len(train[0][i])):
+        #         genCorpus_word.add(train[0][i][j])
+
+        # auto_split = []
+        # W_candidate = set()
+        # for i in range(len(novel_test[0])):
+        #     now_state = b_t.tag(novel_test[2][i],False,b_t.judge_by_rule(novel_test[2][i]))
+        #     auto_split.append(now_state)
+        #     for i in range(2,len(now_state.word)-1):
+        #         if now_state.word[i] not in genCorpus_word:
+        #             W_candidate.add(now_state.word[i]+str(int(now_state.tag[i-1]=='w'and now_state.tag[i+1] =='w')))
+
+        # print(W_candidate)
+
+
+        e_t.prepareKnowledge(train)
+        e_t.weight.weightDict = pickle.load(open(args.enhanced_weight,'rb'))
+
+        e_t.W = w_c.W
+        e_t.P = w_c.P
+
+        ground_true_noun = set()
+        for i,words in enumerate(gold_wordss):
+            for j,w in enumerate(words):
+                if gold_tagss[i][j] in w_c.noun_tags:
+                    ground_true_noun.add(gold_wordss[i][j])
+
+        # e_t.W = ground_true_noun
+
+        novel_gold_state = []
+        for i in range(len(novel_test[0])):
+            novel_gold_state.append(State(novel_test[0][i], novel_test[1][i], isGold=True))
+
+
+        b_result = b_t.test(novel_test[2],novel_gold_state)
+
+        e_result = e_t.test(novel_test[2],novel_gold_state)
+
+        print('base tagger:',b_result)
+        print('\nenhanced tagger:',e_result)
+
+        # print('w:',e_t.wc.lr.w)
+        # print('b:',e_t.wc.lr.b)
+
+    elif args.mode =='tag' :
+        # train,test = loadQiuPKU(args.train,args.data_seed)
+
+        if args.is_raw:
+            # print('israw!')
+            novel_fp = open(args.test, 'r', encoding='utf-8')
+            novel_raw = novel_fp.readlines()
+
+        else:
+            # print('is not raw!')
+            novel_test = loadNovelData(args.test)
+        # novel_fp = open(args.test,'r',encoding='utf-8')
+            novel_raw = novel_test[2]
+
+        train,test = loadQiuPKU(args.train,args.data_seed)
+
+
+        # novel_test = loadNovelData(args.test)
 
 
         b_t.prepareKnowledge(train)
@@ -759,124 +843,19 @@ if __name__ == '__main__':
                 if gold_tagss[i][j] in w_c.noun_tags:
                     ground_true_noun.add(gold_wordss[i][j])
 
-        e_t.W = ground_true_noun
+        # e_t.W = ground_true_noun
 
         novel_gold_state = []
         for i in range(len(novel_test[0])):
             novel_gold_state.append(State(novel_test[0][i], novel_test[1][i], isGold=True))
 
 
-        b_result = b_t.test(novel_test[2],novel_gold_state)
-
-        e_result = e_t.test(novel_test[2],novel_gold_state)
-
-        print('base tagger:',b_result)
-        print('\nenhanced tagger:',e_result)
-
-        # print('w:',e_t.wc.lr.w)
-        # print('b:',e_t.wc.lr.b)
-
-    elif args.mode =='tag' :
-        train,test = loadQiuPKU(args.train,args.data_seed)
-
-        if args.is_raw:
-            # print('israw!')
-            novel_fp = open(args.test, 'r', encoding='utf-8')
-            novel_raw = novel_fp.readlines()
-
-        else:
-            # print('is not raw!')
-            novel_test = loadNovelData(args.test)
-        # novel_fp = open(args.test,'r',encoding='utf-8')
-            novel_raw = novel_test[2]
-
-
-        # for line in novel_raw[:10]:
-        #     print(line)
-        # exit(2)
-
-        for i,line in enumerate(novel_raw):
-            novel_raw[i] = line.strip()
-
-
-        b_t.prepareKnowledge(train)
-        b_t.weight.weightDict = pickle.load(open(args.base_weight,'rb'))
-
-
-
-        # n_count = 0
-        genCorpus_word = set()
-        for i in range(len(train[0])):
-            for j in range(len(train[0][i])):
-                genCorpus_word.add(train[0][i][j])
-
-        auto_split = []
-        W_candidate = set()
-        for i in range(len(novel_raw)):
-            now_state = b_t.tag(novel_raw[i],False,b_t.judge_by_rule(novel_raw[i]))
-            auto_split.append(now_state)
-            for i in range(2,len(now_state.word)-1):
-                if now_state.word[i] not in genCorpus_word:
-                    W_candidate.add(now_state.word[i]+str(int(now_state.tag[i-1]=='w'and now_state.tag[i+1] =='w')))
-
-        # print(W_candidate)
-
-
-        e_t.prepareKnowledge(train)
-        e_t.weight.weightDict = pickle.load(open(args.enhanced_weight,'rb'))
-
-
-        e_t.W = set()
-
-        for p in e_t.P:
-            a,b = p.split('_')
-            onNewPattern(e_t,auto_split,a,b,W_candidate)
-
-
-        # novel_gold_state = []
-        # for i in range(len(novel_test[0])):
-        #     novel_gold_state.append(State(novel_test[0][i], novel_test[1][i], isGold=True))
-
-        # old_result = e_t.test(novel_test[2],novel_gold_state)
-
-        # print('general know:',old_result)
-        W_candidate_copy = copy.deepcopy(W_candidate)
-        noun_tag_set = {'ns','nz','n','nr','nt'}
-        novel_noun_set = set()
-        for i,s in enumerate(test[0]):
-            for j,w in enumerate(test[0][i]):
-                if test[1][i][j] in noun_tag_set:
-                    novel_noun_set.add(test[0][i][j])
-
-        while True:
-            n_count = 0
-            for w in W_candidate:
-                if w[-1] == '1':
-                    t = ['w','w','w']
-                    s = State(['',w[:-1],''],t,True)
-                else:
-                    t = ['','','']
-                    s = State(['',w[:-1],''],t,True)
-
-                isNewNoun = e_t.wc.tag(s,3)
-                # print(w,isNewNoun)
-
-                if isNewNoun>0.5:
-                    print(w, isNewNoun)
-                    W_candidate_copy.remove(w)
-                    e_t.W.add(w[:-1])
-                    n_count+=1
-                    for i in range(len(auto_split)):
-                        for j in range(2,len(auto_split[i].word)-1):
-                            if auto_split[i].word[j] == w[:-1]:
-                                if auto_split[i].word[j-1]+'_'+auto_split[i].word[j+1] not in e_t.P:
-                                    onNewPattern(e_t,auto_split,auto_split[i].word[j-1],
-                                                 auto_split[i].word[j+1],W_candidate)
-            print('\n************\n')
-            if n_count == 0:
-                break
-            n_count = 0
-            W_candidate = copy.deepcopy(W_candidate_copy)
+        # b_result = b_t.test(novel_test[2],novel_gold_state)
+        #
+        # e_result = e_t.test(novel_test[2],novel_gold_state)
+        #
+        # print('base tagger:',b_result)
+        # print('\nenhanced tagger:',e_result)
 
         if args.output_tagged==None:
             f = open('tmp_result','a',encoding='utf-8')
